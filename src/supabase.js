8d244a7f697566305bee8951d1aeee39efe364cc
Troz Pietraszuk <troz@mac.lan>
2026-01-03 22:09:37 -0800

    Updated Supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -4,10 +4,4 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-console.log("VITE_SUPABASE_URL:", import.meta.env.VITE_SUPABASE_URL);
-console.log(
-  "VITE_SUPABASE_ANON_KEY present:",
-  Boolean(import.meta.env.VITE_SUPABASE_ANON_KEY)
-);
-
 export default supabase;
